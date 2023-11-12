76fe6d437dcfaef8a99ac45cf83597a3cd2ce2a6
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-12 17:14:43 +0700

    6.9 created Filter component

---
+++ src/components/Filter.jsx
@@ -0,0 +1,24 @@
+import { useDispatch } from "react-redux"
+import { filterChange } from "../reducers/filterReducer"
+
+const Filter = () => {
+    const dispatch = useDispatch()
+    
+    const handleChange = event => {
+        const content = event.target.value
+        // event.preventDefault()
+        dispatch(filterChange(content))
+    }
+
+    const style = {
+        marginBottom: 10
+    }
+
+    return (
+        <div style={style}>
+            filter <input onChange={handleChange}/>
+        </div>
+    )
+}
+
+export default Filter
