725ec7f129b6e90704984d7e7768d9ddac78bcc1
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-13 16:17:12 +0700

    6.10 uses RTK's createSlice for filter reducer

--- src/reducers/filterReducer.jsx
+++ src/reducers/filterReducer.jsx
@@ -1,18 +1,33 @@
-const filterReducer = (state = '', action) => {
-    switch (action.type) {
-        case 'SET_FILTER':
-            return action.payload
-        default:
-            return state
-    }
-}
+// const filterReducer = (state = '', action) => {
+//     switch (action.type) {
+//         case 'SET_FILTER':
+//             return action.payload
+//         default:
+//             return state
+//     }
+// }
+
+import { createSlice } from "@reduxjs/toolkit"
 
 // Action creator for filter
-export const filterChange = filter => {
-    return {
-        type: 'SET_FILTER',
-        payload: filter.toLowerCase()
-    }
-}
+// export const filterChange = filter => {
+//     return {
+//         type: 'SET_FILTER',
+//         payload: filter.toLowerCase()
+//     }
+// }
 
-export default filterReducer
+const initialState = ''
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState,
+    reducers: {
+        filterChange(state, action) {
+            return action.payload.toLowerCase()
+        }
+    }
+})
+
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
