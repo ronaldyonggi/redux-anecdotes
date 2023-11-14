d0b7850ee164a958c0c5d2f398d8e27211a366af
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-14 12:20:30 +0700

    6.12 voting causes notification to appear

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import { vote } from "../reducers/anecdoteReducer"
+import { setNotification } from "../reducers/notificationReducer"
 
 const Anecdote = ({ anecdote, handleVote}) => {
     return (
@@ -15,10 +16,16 @@ const Anecdote = ({ anecdote, handleVote}) => {
     )
 }
 
+
 const AnecdoteList = () => {
     const dispatch = useDispatch()
     const filter = useSelector(state => state.filter)
     const anecdotes = useSelector(state => state.anecdotes)
+
+    const handleVote = anecdote => {
+        dispatch(setNotification(`you voted '${anecdote.content}'`))
+        dispatch(vote(anecdote.id))
+    }
 
     const filteredAnecdotes = filter.length === 0 
     ? anecdotes
@@ -33,7 +40,7 @@ const AnecdoteList = () => {
                     <Anecdote
                         key = {anecdote.id}
                         anecdote={anecdote}
-                        handleVote={() => dispatch(vote(anecdote.id))}
+                        handleVote={() => handleVote(anecdote)}
                         />
                 ))
             }
