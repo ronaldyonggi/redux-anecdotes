407516f6f1df8bcaa67c9d77d916a3478f5a6a55
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-15 17:51:37 +0700

    6.16 create action to initialize anecdotes

--- src/reducers/anecdoteReducer.jsx
+++ src/reducers/anecdoteReducer.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
+import anecdoteService from '../services/anecdotes'
 
 const anecdotesAtStart = [
   'If it hurts, do it more often',
@@ -39,4 +40,12 @@ const anecdoteSlice = createSlice({
 })
 
 export const { createAnecdote, vote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 export default anecdoteSlice.reducer
