542085452a29176f168ca5111b0fcf9f28a1e5f3
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-16 12:46:45 +0700

    cleaned up commented out parts

--- src/reducers/filterReducer.jsx
+++ src/reducers/filterReducer.jsx
@@ -1,21 +1,4 @@
-// const filterReducer = (state = '', action) => {
-//     switch (action.type) {
-//         case 'SET_FILTER':
-//             return action.payload
-//         default:
-//             return state
-//     }
-// }
-
 import { createSlice } from "@reduxjs/toolkit"
-
-// Action creator for filter
-// export const filterChange = filter => {
-//     return {
-//         type: 'SET_FILTER',
-//         payload: filter.toLowerCase()
-//     }
-// }
 
 const initialState = ''
 
